438389a6f969847044be0aa07895badfdf405120
LI <jasonlee40203006@gmail.com>
2024-02-07 00:42:21 +0800

    9*9 AllFinish

--- src/hw1/HomeWork_9x9.java
+++ src/hw1/HomeWork_9x9.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class HomeWork_9x9 {
 
 	@Test//9*9乘法表
-	public void method04() {
+	public void method00() {
 		int j,k;
 		
 		for(j=1;j<=9;j++) {
@@ -16,14 +16,44 @@ public class HomeWork_9x9 {
 		}
 	}
 	
-	@Test//for+do while
-	public void method05() {
-		
+	
+	@Test//for+while
+	public void method01() {
+		int j;
+		for(int i=1;i<=9;i++){
+			j=1;
+			while(j<=9) {
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+				j++;
+				}
+			System.out.println();
+			}
 	}
 	
-	@Test
-	public void method06() {
-		
-		
+	@Test//for+do while
+	public void method02() {
+		for(int i=1;i<=9;i++) {
+			int j=1;
+			do {
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+				j++;
+			}while(j<=9);
+			System.out.println();
+		}
+	}
+	
+	@Test//while+do while
+	public void method03() {
+		int i=1,j=1;
+		while(i<=9) {
+			do {
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+				j++;
+			}while(j<=9);{
+				System.out.println();
+				i++;
+				j=1;
+			}	
+		}
 	}
 }
