ee9738b993d11663bd7dce51b34a98bb9cacf258
LI <jasonlee40203006@gmail.com>
2024-02-15 21:17:56 +0800

    HomeWork0303

--- src/hw3/HomeWork0303.java
+++ src/hw3/HomeWork0303.java
@@ -10,12 +10,13 @@ public class HomeWork0303 {
 	@Test
 	public void method00() {
 		int a=0;
+		int[] num=new int[1];
 		int[][] choies=new int[7][7];
 		Scanner sc=new Scanner(System.in);
 		System.out.println("請輸入你討厭的數字(1~9)");
 		if(sc.hasNextInt()) {
-			int num=sc.nextInt();
-			if(num>9||num<1) {
+			 num[0]=sc.nextInt();
+			if(num[0]>9||num[0]<1) {
 				System.out.println("請重新輸入!");
 			}
 			else {
@@ -23,14 +24,14 @@ public class HomeWork0303 {
 				for(int i=0;i<choies.length;i++) {
 					for(int j=0;j<choies[i].length;j++) {
 						choies[i][j]=++a;
-						if(a!=num) {
+						if(a!=num[0]) {
 							System.out.print(a+" ");
 							}
 						
 					}
 					System.out.println("");
 				}
-				System.out.println("共有"+(a-num)+"個號碼可以選");
+				System.out.println("共有"+(a-num.length)+"個號碼可以選");
 			}
 		}else {
 			System.out.println("請重新輸入!");
@@ -38,7 +39,7 @@ public class HomeWork0303 {
 	}
 	
 	
-	@Test//pro 選完產生六個不重複
+	@Test//pro 選完產生六個不重複 //!未完成!
 	public void method01() {
 		int a=0;
 		int[] random=new int[6];
@@ -61,6 +62,7 @@ public class HomeWork0303 {
 							System.out.print(a+" ");
 						}
 					}
+					System.out.println();
 				}
 			}
 		}else {
